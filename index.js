07c5246478815b5342cc0043d7e183c8528d6563
IORD1 <pratham111ingole@gmail.com>
2022-12-02 14:32:32 +0530

    firebase intiated

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfcELjuq5W4MdUABqxDh9j1c4OTXTlTYI",
+  authDomain: "finhack2.firebaseapp.com",
+  projectId: "finhack2",
+  storageBucket: "finhack2.appspot.com",
+  messagingSenderId: "366774345785",
+  appId: "1:366774345785:web:d88fea9b91b7bb712aade8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
